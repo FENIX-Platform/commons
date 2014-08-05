99015da449f08296abc2526a28ca4e2252cbb45b
Ivano Cortesini <ivano.cortesini@gmail.com>
2014-08-05 18:06:21 +0200

    ...

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/data/ResourceProxy.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/data/ResourceProxy.java
@@ -1,5 +1,6 @@
 package org.fao.fenix.commons.msd.dto.data;
 
+import javassist.util.proxy.Proxy;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.fao.fenix.commons.msd.dto.templates.ResponseBeanFactory;
 import org.fao.fenix.commons.msd.dto.templates.ResponseHandler;
@@ -10,21 +11,27 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Map;
 
-public class ResourceProxy <M extends ResponseHandler, D extends ResponseHandler> {
+public class ResourceProxy <M extends ResponseHandler, D> {
 
 
-    public static <M extends ResponseHandler, D extends ResponseHandler, S> Collection<ResourceProxy<M,D>> getInstances(Collection<Resource<S>> sources, Class<M> metadataProxyClass, Class<D> dataProxyClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public static <M extends ResponseHandler, D, S> Collection<ResourceProxy<M,D>> getInstances(Collection<Resource<S>> sources, Class<M> metadataProxyClass, Class<D> dataProxyClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Collection<ResourceProxy<M,D>> proxies = new LinkedList<>();
         if (sources!=null)
             for (Resource source : sources)
                 proxies.add(getInstance(source,metadataProxyClass,dataProxyClass));
         return proxies;
     }
-    public static <M extends ResponseHandler, D extends ResponseHandler, S> ResourceProxy getInstance(Resource<S> source, Class<M> metadataProxyClass, Class<D> dataProxyClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        return source==null ? null : new ResourceProxy(
-            ResponseBeanFactory.getInstance(source.getMetadata(), metadataProxyClass),
-            ResponseBeanFactory.getInstances(source.getData(), dataProxyClass)
-        );
+    public static <M extends ResponseHandler, D, S> ResourceProxy getInstance(Resource<S> source, Class<M> metadataProxyClass, Class<D> dataProxyClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        if (source!=null)
+            if (ResponseHandler.class.isAssignableFrom(dataProxyClass))
+                return new ResourceProxy(
+                    ResponseBeanFactory.getInstance(source.getMetadata(), metadataProxyClass),
+                    ResponseBeanFactory.getInstances(source.getData(), (Class<? extends ResponseHandler>)dataProxyClass)
+                );
+            else
+                return new ResourceProxy( ResponseBeanFactory.getInstance(source.getMetadata(), metadataProxyClass), source.getData() );
+        else
+            return null;
     }
 
     private M metadata;
