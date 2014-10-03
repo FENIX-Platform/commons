faafba5a1a6c4a13f3adb6e57124750639419925
Ivano Cortesini <ivano.cortesini@gmail.com>
2014-10-03 17:29:48 +0200

    ...

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/templates/codeList/Code.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/templates/codeList/Code.java
@@ -4,6 +4,7 @@ import org.codehaus.jackson.annotate.JsonProperty;
 import org.fao.fenix.commons.msd.dto.templates.ResponseHandler;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Code extends ResponseHandler {
@@ -36,6 +37,23 @@ public class Code extends ResponseHandler {
     }
     @JsonProperty
     public Collection<Code> getChildren() {
+        Integer currentLevel = getLevel();
+        Integer[] levelParameters = levelInfo.get();
+        if (currentLevel!=null && levelParameters!=null) {
+            Integer level = levelParameters[0];
+            Integer levels = levelParameters[1];
+            Integer levelIndex = levelParameters[2];
+            if (level!=null && currentLevel<level)
+                return new LinkedList<>();
+            if (levels!=null)
+                if (levelIndex>=levels)
+                    return new LinkedList<>();
+                else
+                    levelParameters[2] = levelIndex+1;
+        }
         return null;
     }
+
+    //Levels filter support
+    public static ThreadLocal<Integer[]> levelInfo = new ThreadLocal<>();
 }
