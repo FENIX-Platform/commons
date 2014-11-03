b6b9eacc990406ca1738f629456ef39f3e9f2f3f
ivanoFenix <ivano.cortesini@gmail.com>
2014-11-03 08:48:39 +0100

    ...

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/data/TimeFilter.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/data/TimeFilter.java
@@ -1,10 +1,9 @@
 package org.fao.fenix.commons.msd.dto.data;
 
-import java.util.Date;
 
 public class TimeFilter {
 
-    public Date from;
-    public Date to;
+    public Long from;
+    public Long to;
 
 }
