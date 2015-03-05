b38656fdd114abada208c005ccb44e7e2e8fe85e
francescafenix <francescarosa88@gmail.com>
2015-03-05 16:00:24 +0100

    Update SeCodeList.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeCodeList.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeCodeList.java
@@ -11,13 +11,13 @@ import java.io.Serializable;
 public class SeCodeList extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="")
-    @Description(en= "")
+    @Label(en="Number of levels")
+    @Description(en= "Hierarchical codelists may have several levels, information about the number of levels must be reported in order to describe the hierchy of the codelist.")
     private Integer numberOfLevels;
 
     @JsonProperty
-    @Label(en="")
-    @Description(en= "")
+    @Label(en="Type of codelist")
+    @Description(en= "Codelist typology. A codelist can be a simple objects basically composed by a list of couples of code and label or a more complex object whose elements present some hierarchical relationships.")
     private CodeListType typeOfCodeList;
 
 
