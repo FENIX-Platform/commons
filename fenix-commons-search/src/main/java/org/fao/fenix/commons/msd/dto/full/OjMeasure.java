a53f0b4848f8a110d3fbc0e83298304632328f1e
francescafenix <francescarosa88@gmail.com>
2015-03-19 11:33:05 +0100

    Update OjMeasure.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/OjMeasure.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/OjMeasure.java
@@ -11,28 +11,28 @@ import java.util.Map;
 public class OjMeasure extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="")
-    @Description(en="")
+    @Label(en="Extent")
+    @Description(en="Description of the type of magnitude has been measured (e.g. a length, an area, a volume).")
     private Map<String, String> extent;
 
     @JsonProperty
-    @Label(en="")
-    @Description(en="")
+    @Label(en="Composed unit of measurement")
+    @Description(en="Since a wide variety of phenomenon is measured combining several simple unit of measure, it is requested a Boolean element indicating whether simple or composed unit of measure is used.")
     private Boolean composedMU;
 
     @JsonProperty
-    @Label(en="")
-    @Description(en="")
+    @Label(en="Measurement system")
+    @Description(en="System devised to measure some physical quantity (such distance or area. . . ). Example are the English System of feet and inches or the metric system of millimetres, centimetres and meters, also the International System of Units (SI).")
     private Map<String, String> measurementSystem;
 
     @JsonProperty
-    @Label(en="")
-    @Description(en="")
+    @Label(en="Measurement unit name")
+    @Description(en="Name of the simple/composed unit of measure adopted.")
     private String nameMU;
 
     @JsonProperty
-    @Label(en="")
-    @Description(en="")
+    @Label(en="Conversion to the standard")
+    @Description(en="Allows to convert the unit of measure used in a standard measurement system.")
     private Double conversionToStandard;
 
 
