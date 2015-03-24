a46cf4f44dcafe7989d65708612f1a3011b2ee8f
francescafenix <francescarosa88@gmail.com>
2015-03-24 19:39:18 +0100

    Update MeSpatialRepresentation.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeSpatialRepresentation.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeSpatialRepresentation.java
@@ -16,35 +16,35 @@ public class MeSpatialRepresentation extends JSONEntity implements Serializable 
     @JsonProperty
     @Label(en="Type of layer")
     @Description(en= "Layer typology: vector or raster spatial representation.")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private LayerType layerType;
 
     @JsonProperty
     @Label(en="Type of product")
     @Description(en= "Coded information about the product managed.")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private OjCodeList typeOfProduct;
 
     @JsonProperty
-    @Label(en="")
+    @Label(en="Processing")
     @Description(en= "")
-    @Order(-1)
+    @Order(3)
     @Format(Format.FORMAT.string)
     private OjCodeList processing;
 
     @JsonProperty
     @Label(en="Value assigned to No-data")
     @Description(en= "Value assigned to the cells to represent the absence of data.")
-    @Order(-1)
+    @Order(4)
     @Format(Format.FORMAT.string)
     private String noDataValue;
 
     @JsonProperty
     @Label(en="Bounding Box")
     @Description(en= "This section includes the planar measurement units used to express the X,Y values. The bounding box indicates the smaller quadrilateral shape that includes all features of the considered resource.")
-    @Order(-1)
+    @Order(5)
     @Format(Format.FORMAT.string)
     private SeBoundingBox seBoundingBox;
 
