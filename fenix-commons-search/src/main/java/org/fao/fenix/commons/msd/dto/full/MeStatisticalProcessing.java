56ab747a0430b5c7713b05dedd2b1b82472beebb
francescafenix <francescarosa88@gmail.com>
2015-03-24 16:18:44 +0100

    Update MeStatisticalProcessing.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeStatisticalProcessing.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeStatisticalProcessing.java
@@ -15,21 +15,21 @@ public class MeStatisticalProcessing extends JSONEntity implements Serializable 
     @JsonProperty
     @Label(en="Data source")
     @Description(en= "Process used to collect data. It includes a detailed description both of the primary data collection (e.g. type of collection, method to gather data from respondents, sampling procedures..) and the secondary data collection (information about data that have been already collected by another agency or institution).")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private SeDataSource seDataSource;
 
     @JsonProperty
     @Label(en="Data compilation")
     @Description(en= "This section describes the main statistics actions operated on data (e.g. data editing, imputation, weighting, adjustment for non-response, model used etc.).")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private SeDataCompilation seDataCompilation;
 
     @JsonProperty
     @Label(en="Data Validation")
     @Description(en= "Methods and processes for assessing statistical data. It describes the process of monitoring data compilation progress and ensuring the quality of the statistical results.")
-    @Order(-1)
+    @Order(3)
     @Format(Format.FORMAT.string)
     private SeDataValidation seDataValidation;
 
