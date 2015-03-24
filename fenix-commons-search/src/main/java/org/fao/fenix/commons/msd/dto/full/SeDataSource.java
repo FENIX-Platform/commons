3804980f4429614a2b9cf2027aa27266485a3c5e
francescafenix <francescarosa88@gmail.com>
2015-03-24 19:31:50 +0100

    Update SeDataSource.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeDataSource.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeDataSource.java
@@ -15,14 +15,14 @@ public class SeDataSource extends JSONEntity implements Serializable {
     @JsonProperty
     @Label(en="Primary Data Collection")
     @Description(en= "This section describes the procedures used to collect data obtained directly from first-hand sources by means of surveys, observation or experimentation.")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private SePrimaryDataCollection sePrimaryDataCollection;
 
     @JsonProperty
     @Label(en="Secondary Data Collection")
     @Description(en= "This section is filled when the agency compiling and publishing data does not coincide with the entity (subject, agency or institution) who has conducted the procedure of collecting data. It reports information about the source that have already collected data.")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private SeSecondaryDataCollection seSecondaryDataCollection;
 
