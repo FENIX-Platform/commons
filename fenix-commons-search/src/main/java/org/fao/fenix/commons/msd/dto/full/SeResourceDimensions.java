568ccd1cb77ef8d4c29a2d858361188725898af0
francescafenix <francescarosa88@gmail.com>
2015-03-24 19:29:53 +0100

    Update SeResourceDimensions.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeResourceDimensions.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeResourceDimensions.java
@@ -18,35 +18,35 @@ public class SeResourceDimensions extends JSONEntity implements Serializable {
     @JsonProperty
     @Label(en="Dimension name")
     @Description(en= "Name of the dimension as indicated in the resource.")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private Map<String, String> dimensionName;
 
     @JsonProperty
     @Label(en="Dimension type")
     @Description(en= "Data type of the dimension (e.g. free-text, coded).")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private DimensionType dimensionType;
 
     @JsonProperty
     @Label(en="Dimension type")
     @Description(en= "Coded element that indicates the CodeList main subject-theme.")
-    @Order(-1)
+    @Order(3)
     @Format(Format.FORMAT.string)
     private OjCodeList dimensionSubject;
 
     @JsonProperty
     @Label(en="Dimension content")
     @Description(en= "List of the values of the considered dimension occurring in the resource.")
-    @Order(-1)
+    @Order(4)
     @Format(Format.FORMAT.string)
     private Object dimensionContent;
 
     @JsonProperty
     @Label(en="Measurement units")
     @Description(en= "Measurement units of the data values.")
-    @Order(-1)
+    @Order(5)
     @Format(Format.FORMAT.string)
     private OjMeasure dimensionMU;
 
