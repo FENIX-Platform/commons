7a6103384234cd97948fba6aec46816bf93cae26
francescafenix <francescarosa88@gmail.com>
2015-03-24 18:54:48 +0100

    Update SeDataRevision.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeDataRevision.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeDataRevision.java
@@ -16,15 +16,15 @@ public class SeDataRevision extends JSONEntity implements Serializable {
     @JsonProperty
     @Label(en="Revision policy")
     @Description(en= "Policy concerning the periodically revision of the resource and ensuring the transparency of disseminated data.")
-    @Order(-1)
-    @Format(Format.FORMAT.string)
+    @Order(1)
+    @Format(Format.FORMAT.textarea)
     private Map<String, String> revisionPolicy;
 
     @JsonProperty
     @Label(en="Revision practice")
     @Description(en= "Information concerning the revision of data in order to give compilers the possibility of incorporating new and more accurate information in the resource. It also describes the revision status of available data. Data may also be subject to regular or ad hoc revisions as a result of the introduction of new classification, compilation frameworks and methodologies in order to improve the accuracy of the resource.")
-    @Order(-1)
-    @Format(Format.FORMAT.string)
+    @Order(2)
+    @Format(Format.FORMAT.textarea)
     private Map<String, String> revisionPractice;
 
 
