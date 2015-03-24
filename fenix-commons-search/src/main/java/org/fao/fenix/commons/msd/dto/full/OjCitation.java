436377bcfcc2e77a31ee2ae8533afa99bc9a81eb
francescafenix <francescarosa88@gmail.com>
2015-03-24 19:28:46 +0100

    Update OjCitation.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/OjCitation.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/OjCitation.java
@@ -18,56 +18,56 @@ public class OjCitation extends JSONEntity implements Serializable {
     @JsonProperty
     @Label(en="Kind of document")
     @Description(en="Kind of resource attached to data.")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private DocumentType documentKind;
 
     @JsonProperty
     @Label(en="Title")
     @Description(en="Title by which the cited resource is known which offers a quick information about its content.")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private Map<String, String> title;
 
     @JsonProperty
     @Label(en="Date")
     @Description(en="Reference date for the cited document.")
-    @Order(-1)
-    @Format(Format.FORMAT.string)
+    @Order(3)
+    @Format(Format.FORMAT.date)
     private Date date;
 
     @JsonProperty
     @Label(en="Contact")
     @Description(en="Name and position information for an individual or organization that is responsible for the resource.")
-    @Order(-1)
+    @Order(4)
     @Format(Format.FORMAT.string)
     private OjResponsibleParty documentContact;
 
     @JsonProperty
     @Label(en="Notes")
     @Description(en="Some additional details about the resource mainly concerning the domain to which the resource refers.")
-    @Order(-1)
-    @Format(Format.FORMAT.string)
+    @Order(5)
+    @Format(Format.FORMAT.textarea)
     private Map<String, String> notes;
 
     @JsonProperty
     @Label(en="Resource hyperlink")
     @Description(en="Web site link allowing the user access to the information resource.")
-    @Order(-1)
-    @Format(Format.FORMAT.string)
+    @Order(6)
+    @Format(Format.FORMAT.url)
     private String link;
 
     @JsonProperty
     @Label(en="ISBN code")
     @Description(en="If available, the International Standard Book Number can be reported in order to uniquely identify the resource cited.")
-    @Order(-1)
+    @Order(7)
     @Format(Format.FORMAT.string)
     private String isbn;
 
     @JsonProperty
     @Label(en="ISNN code")
     @Description(en="If available, the International Standard Serial Number (used to identify periodical publications such as reviews and scientific journals) can be reported in order to uniquely identify the resource cited.")
-    @Order(-1)
+    @Order(8)
     @Format(Format.FORMAT.string)
     private String issn;
 
