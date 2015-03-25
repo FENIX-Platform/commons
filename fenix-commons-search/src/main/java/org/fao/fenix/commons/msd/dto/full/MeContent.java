b91a4cff25c63c1f58146ba195fd0ed952abe9be
francescafenix <francescarosa88@gmail.com>
2015-03-25 15:46:56 +0100

    Update MeContent.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeContent.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/MeContent.java
@@ -16,49 +16,49 @@ import java.util.Map;
 public class MeContent extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="Type of resource")
+    @Label(en="Type of resource",fr="Type de ressource",es="Tipo de recurso")
     @Description(en= "Typology of the resource the metadata refers to. This metadata element determines whether certain meta-data entities are, or are not applicable. For example, the metadata entities 'SpatialRepresentation' and 'ReferenceSystem' are only available for geospatial resource types (e.g. raster; vector).")
     @Order(1)
     @Format(Format.FORMAT.string)
     private RepresentationType resourceRepresentationType;
 
     @JsonProperty
-    @Label(en="Keywords")
+    @Label(en="Keywords",fr="Mots-clés",es="Palabras clave")
     @Description(en= "Commonly used word(s), formalized word(s) or phrase(s) used to describe the resource.")
     @Order(2)
     @Format(Format.FORMAT.string)
     private Collection<String> keywords;
 
     @JsonProperty
-    @Label(en="Abstract")
+    @Label(en="Abstract",fr="Résumé",es="Resumen")
     @Description(en= "Overview of the main characteristics of the resource and summary of the information contained in the resource, in an easily understandable manner, for technical and non-technical users.")
     @Order(3)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> description;
 
     @JsonProperty
-    @Label(en="Statistical concepts / definitions")
+    @Label(en="Statistical concepts / definitions",fr="Concepts statistiques / définitions",es="Conceptos estadísticos y / o definiciones")
     @Description(en= "Definitions of the statistical concepts under measure (i.e. the statistical domain) and the main variables provided. The considered types of variables (e.g. raw figures, annual growth rates, index, ow or stock data, ...) should be defined and described in accordance with internationally accepted statistical standards, guidelines, or good practices.")
     @Order(4)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> statisticalConceptsDefinitions;
 
-    @JsonProperty
-    @Label(en="Reference population")
+    @JsonPropertyPopulation Reference
+    @Label(en="Reference population",fr="Population de Reference",es="Population Reference")
     @Description(en= "Information about the statistical population the resource refers to.")
     @Order(5)
     @Format(Format.FORMAT.string)
     private SeReferencePopulation seReferencePopulation;
 
     @JsonProperty
-    @Label(en="Coverage")
+    @Label(en="Coverage",fr="Couverture",es="Cobertura")
     @Description(en= "Size and extent of the resource. The term 'coverage' encompasses the descriptions of key dimensions delimiting the data, e.g. geographical, institutional, product, economic sector,etc., as well as relevant exceptions and exclusions.")
     @Order(6)
     @Format(Format.FORMAT.string)
     private SeCoverage seCoverage;
 
     @JsonProperty
-    @Label(en="Codelist")
+    @Label(en="Codelist",fr="",es="")
     @Description(en= "This section allows to report information specifically addressed to resources of the kind 'codelist'.")
     @Order(7)
     @Format(Format.FORMAT.string)
