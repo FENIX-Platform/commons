a350dfcb14d29f8607a928497266d7e61f7e3eed
francescafenix <francescarosa88@gmail.com>
2015-03-25 11:22:42 +0100

    Update SeEllipsoidParameters.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeEllipsoidParameters.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeEllipsoidParameters.java
@@ -15,21 +15,21 @@ public class SeEllipsoidParameters extends JSONEntity implements Serializable {
     @JsonProperty
     @Label(en="Semi major axis")
     @Description(en= "Measure of the equatorial axis of the ellipsoid.")
-    @Order(-1)
+    @Order(1)
     @Format(Format.FORMAT.string)
     private Double semiMajorAxis;
 
     @JsonProperty
     @Label(en="Semi-major axis unit of measure")
     @Description(en= "Units of the semi-major axis.")
-    @Order(-1)
+    @Order(2)
     @Format(Format.FORMAT.string)
     private OjMeasure axisUnits;
 
     @JsonProperty
     @Label(en="Deniminator of flattening rate")
     @Description(en= "Ratio between the semi-major axis and the semi-minor axis of an ellipse. Generally an ellipse is completely specified by the measure of the semi-major axis 'a' and the measure of the semi-minor axis 'b'.")
-    @Order(-1)
+    @Order(3)
     @Format(Format.FORMAT.string)
     private Double denominatorOfFlatteringRatio;
 
