54c394999f7e8a1ba41c98aaf0aa65c06ef622e3
francescafenix <francescarosa88@gmail.com>
2015-03-26 15:59:42 +0100

    Update SeConfidentiality.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeConfidentiality.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeConfidentiality.java
@@ -14,21 +14,21 @@ import java.util.Map;
 public class SeConfidentiality extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="Confidentiality - Policy")
+    @Label(en="Confidentiality - Policy",fr="Confidentialité - Politique",es="Confidencialidad - Política")
     @Description(en= "Legislative measures or other formal procedures which prevent unauthorized disclosure of data that identify a person or economic entity either directly or indirectly. It consists in textual description and references to legislation or other rules related to statistical confidentiality.")
     @Order(1)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> confidentialityPolicy;
 
     @JsonProperty
-    @Label(en="Confidentiality - Data treatment")
+    @Label(en="Confidentiality - Data treatment",fr="Confidentialité - Traitement des données",es="Tratamiento de datos - Privacidad")
     @Description(en= "Rules applied for treating the resource to ensure confidentiality and prevent unauthorized disclosure.")
     @Order(2)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> confidentialityDataTreatment;
 
     @JsonProperty
-    @Label(en="Status of confidentiality")
+    @Label(en="Status of confidentiality",fr="Statut de la confidentialité",es="Estado de confidencialidad")
     @Description(en= "Coded information describing the degree of confidentiality of the resource.")
     @Order(3)
     @Format(Format.FORMAT.string)
