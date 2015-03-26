f9c81322839434095fc94042fcfa1a5b049693f9
francescafenix <francescarosa88@gmail.com>
2015-03-26 15:12:32 +0100

    Update SeComparability.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeComparability.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeComparability.java
@@ -13,21 +13,21 @@ import java.util.Map;
 public class SeComparability extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="Geographic comparability")
+    @Label(en="Geographic comparability",fr="Comparabilité géographique",es="Comparabilidad temporal")
     @Description(en= "Degree of data comparability across the geographic areas or regions referenced by the resource. Data might be derived from surveys that in general are conducted by different statistical agencies. These surveys often refer to populations of different geographical areas, sometimes based on different methodologies.")
     @Order(1)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> comparabilityGeographical;
 
     @JsonProperty
-    @Label(en="Time comparability")
+    @Label(en="Time comparability",fr="Comparabilité du temps",es="Comparabilidad Tiempo")
     @Description(en= "Extent to which data are comparable or reconcilable over time. It refers to the degree of comparability between the measures of a time series (e.g. related to a country, a commodity and a variable) included in the resource.")
     @Order(2)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> comparabilityTime;
 
     @JsonProperty
-    @Label(en="Internal coherence")
+    @Label(en="Internal coherence",fr="Cohérence interne",es="Coherencia interna")
     @Description(en= "General estimate of the extent to which data are consistent within the resource.")
     @Order(3)
     @Format(Format.FORMAT.textarea)
