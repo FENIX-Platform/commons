c6510de62d067dc0159e39e7158893d33ed7e9a5
francescafenix <francescarosa88@gmail.com>
2015-03-26 15:10:44 +0100

    Update SeRelevance.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeRelevance.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeRelevance.java
@@ -13,28 +13,28 @@ import java.util.Map;
 public class SeRelevance extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="User needs")
+    @Label(en="User needs",fr="Besoins des utilisateurs",es="Necesidades de los usuarios")
     @Description(en= "Classification of users with some indication of their importance, an indication of the uses for which they want the statistical outputs and as well users and uses given special considerations. Unmet user needs and the reasons for not meeting them should be included as well.")
     @Order(1)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> userNeeds;
 
     @JsonProperty
-    @Label(en="User satisfaction")
+    @Label(en="User satisfaction",fr="Satisfaction des utilisateurs",es="La satisfacción del usuario")
     @Description(en= "How the views and opinions of the users are collected. In addition the main results regarding the user satisfaction should be shown (in the form of a user satisfaction index if available) and the date of most recent user satisfaction survey.")
     @Order(2)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> userSatisfaction;
 
     @JsonProperty
-    @Label(en="Completeness")
+    @Label(en="Completeness",fr="Exhaustivité",es="Exhaustividad")
     @Description(en= "State of completeness of the resource.")
     @Order(3)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> completeness;
 
     @JsonProperty
-    @Label(en="Percentage of completeness")
+    @Label(en="Percentage of completeness",fr="Pourcentage d'exhaustivité",es="Porcentaje de integridad")
     @Description(en= "Percentage of the state of completeness of the resource.")
     @Order(4)
     @Format(Format.FORMAT.string)
