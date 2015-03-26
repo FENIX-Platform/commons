7eeedfe1f88476cf5b4e2fadf881b233f7aef8d3
francescafenix <francescarosa88@gmail.com>
2015-03-26 14:59:25 +0100

    Update SeAccuracy.java

--- fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeAccuracy.java
+++ fenix-commons-search/src/main/java/org/fao/fenix/commons/msd/dto/full/SeAccuracy.java
@@ -13,14 +13,14 @@ import java.util.Map;
 public class SeAccuracy extends JSONEntity implements Serializable {
 
     @JsonProperty
-    @Label(en="Accuracy - non sampling error")
+    @Label(en="Accuracy - non sampling error",fr="Précision - erreur pas due au échantillonnage",es="Precisión - sin error de muestreo")
     @Description(en= "Error in sample estimates which cannot be attributed to sampling fluctuations. (e.g. defects in the sampling frame, faulty demarcation of sample units, defects in the selection of sample units, mistakes in the collection of data due to personal variations, misunderstanding, bias, negligence . . . etc.)")
     @Order(1)
     @Format(Format.FORMAT.textarea)
     private Map<String, String> accuracyNonSampling;
 
     @JsonProperty
-    @Label(en="Accuracy - sampling error")
+    @Label(en="Accuracy - sampling error",fr="Précision - erreur d'échantillonnage",es="Precisión - error de muestreo")
     @Description(en= "If probability sampling is used, the accuracy is an evaluation of difference between a population value and an estimate thereof, derived from a random sample (so due to the fact that only a subset of the population is enumerate), normally in the form of coefficient of variation, standard error or confidence intervals. For non-probability sampling, random errors cannot be calculated without reference to some kind of model, in this case estimates of the accuracy, a motivation for the invoked model for this estimation and brief discussion of sampling bias should be provided.")
     @Order(2)
     @Format(Format.FORMAT.textarea)
