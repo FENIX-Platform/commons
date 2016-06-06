659eb427bbde9c35270622bd443c5819e4738a6a
ivanoFenix <ivano.cortesini@gmail.com>
2016-06-06 16:53:21 +0200

    ...

--- fenix-commons-utils/src/main/java/org/fao/fenix/commons/utils/FileUtils.java
+++ fenix-commons-utils/src/main/java/org/fao/fenix/commons/utils/FileUtils.java
@@ -4,8 +4,13 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 @ApplicationScoped
 public class FileUtils {
@@ -19,7 +24,7 @@ public class FileUtils {
     }
 
     public void delete(File toDelete) {
-        if (toDelete.exists()) {
+        if (toDelete!=null && toDelete.exists()) {
             if (toDelete.isDirectory())
                 for (File f:toDelete.listFiles())
                     delete(f);
@@ -47,5 +52,89 @@ public class FileUtils {
         out.close();
     }
 
+
+
+    public void unzip(File source, File destination, boolean overwrite) throws IOException {
+        if (source==null || !source.exists() || !source.isFile())
+            throw new FileNotFoundException(source!=null ? source.getAbsolutePath() : null);
+        unzip(new FileInputStream(source), destination, overwrite);
+    }
+    public void unzip(InputStream source, File destination, boolean overwrite) throws IOException {
+        if (destination==null || destination.isFile())
+            throw new FileNotFoundException("<destination>");
+        if (!destination.exists())
+            destination.mkdirs();
+
+        ZipInputStream in = new ZipInputStream(source);
+        try {
+            for (ZipEntry entry = in.getNextEntry(); entry != null; entry = in.getNextEntry()) {
+                File entryFile = new File(destination, entry.getName());
+                if (entryFile.exists())
+                    if (overwrite)
+                        delete(entryFile);
+                    else
+                        throw new IOException("Cannot overwrite an existing destination file: " + entryFile.getAbsolutePath());
+
+                if (entry.isDirectory())
+                    entryFile.mkdirs();
+                else {
+                    OutputStream out = new FileOutputStream(entryFile);
+                    copy(out, in);
+                    out.close();
+                }
+            }
+        } finally {
+            if (in!=null)
+                in.close();
+        }
+    }
+
+    public void zip(File source, File destination, boolean overwrite) throws IOException {
+        if (destination==null)
+            throw new FileNotFoundException("<destination>");
+        if (destination.exists())
+            if (overwrite)
+                delete(destination);
+            else
+                throw new IOException("Cannot overwrite an existing destination file: "+destination.getAbsolutePath());
+
+        zip(source, new FileOutputStream(destination), overwrite);
+    }
+    public void zip(File source, OutputStream destination, boolean overwrite) throws IOException {
+        if (source==null || !source.exists())
+            throw new FileNotFoundException(source!=null ? source.getAbsolutePath() : null);
+
+        ZipOutputStream out = new ZipOutputStream(destination);
+        try {
+            zip(source, out, "");
+        } finally {
+            if (out!=null)
+                out.close();
+        }
+    }
+    private void zip(File source, ZipOutputStream out, String entryPath) throws IOException {
+        entryPath += source.getName() + (source.isDirectory() ? File.separatorChar : "");
+        try {
+            out.putNextEntry(new ZipEntry(entryPath));
+
+            if (source.isFile()) {
+                InputStream in = new FileInputStream(source);
+                copy(out, in);
+                in.close();
+            } else
+                for (File child : source.listFiles())
+                    zip(child, out, entryPath);
+
+            out.closeEntry();
+        } catch (ZipException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    private void copy (OutputStream out, InputStream in) throws IOException {
+        byte[] buffer = new byte[1024];
+        for (int c=in.read(buffer); c>0; c=in.read(buffer))
+            out.write(buffer,0,c);
+        out.flush();
+    }
 
 }
