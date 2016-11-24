218576f48cf27ce80899af707f462e86a42f13b3
ivanocortesini <ivano.cortesini@gmail.com>
2016-11-24 14:38:00 +0100

    ...

--- fenix-commons-dxf/src/main/java/org/fao/fenix/commons/find/dto/filter/NumberFilter.java
+++ fenix-commons-dxf/src/main/java/org/fao/fenix/commons/find/dto/filter/NumberFilter.java
@@ -3,7 +3,7 @@ package org.fao.fenix.commons.find.dto.filter;
 
 public class NumberFilter {
 
-    public Long from; //seconds when filter metadata
-    public Long to; //seconds when filter metadata
+    public Double from;
+    public Double to;
 
 }
