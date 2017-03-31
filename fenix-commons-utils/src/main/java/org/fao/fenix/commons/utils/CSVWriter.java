4694477e25374643be70cc6024db2d59d3e0859c
fabriziofenix <fabrizio.castelli@fao.org>
2017-03-31 19:17:59 +0200

    csv

--- fenix-commons-utils/src/main/java/org/fao/fenix/commons/utils/CSVWriter.java
+++ fenix-commons-utils/src/main/java/org/fao/fenix/commons/utils/CSVWriter.java
@@ -107,4 +107,80 @@ public class CSVWriter {
     }
 
 
+    public int write(FileWriter fileWriter, Iterator<Object[]> data, int size) throws Exception {
+        if (writeHeader) {
+            writeHeader=false;
+            writeHeader();
+        }
+        StringBuilder row = new StringBuilder();
+        int count = 0;
+
+        if (data!=null)
+            while (data.hasNext() && count<size) {
+                for (Object cell : data.next()) {
+                    if (cell==null)
+                        row.append(separator);
+                    else if (cell instanceof Number)
+                        row.append(numberFormatter.format(cell)).append(separator);
+                    else if (cell instanceof Date)
+                        row.append(dateFormatter.format(cell)).append(separator);
+                    else {
+                        if (useQuote)
+                            row.append('"').append(cell.toString().replaceAll("\"", "\\\\\"")/*.replaceAll(";", "\\"+separator+"")*/).append('"').append(separator);
+                        else
+                            row.append(cell.toString()).append(separator);
+                    }
+                }
+                if (windows) {
+                    row.setCharAt(row.length() - 1, '\r');
+                    row.append('\n');
+                } else
+                    row.setCharAt(row.length()-1,'\n');
+                fileWriter.write(row.toString());
+                row.setLength(0);
+                count++;
+            }
+
+        fileWriter.flush();
+        if (close)
+            fileWriter.close();
+
+        return count;
+    }
+
+
+    public static  Object deepCopy(Object oldObj) throws Exception
+    {
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try
+        {
+            ByteArrayOutputStream bos =
+                    new ByteArrayOutputStream(); // A
+            oos = new ObjectOutputStream(bos); // B
+            // serialize and pass the object
+            oos.writeObject(oldObj);   // C
+            oos.flush();               // D
+            ByteArrayInputStream bin =
+                    new ByteArrayInputStream(bos.toByteArray()); // E
+            ois = new ObjectInputStream(bin);                  // F
+            // return the new object
+            return ois.readObject(); // G
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception in ObjectCloner = " + e);
+            throw(e);
+        }
+        finally
+        {
+            oos.close();
+            ois.close();
+        }
+    }
+
+
+
+
+
 }
